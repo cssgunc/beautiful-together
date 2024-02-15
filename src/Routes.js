9d70d6d6a2a208e4ae5a4ebb64d49911b70fe2bf
Nirmal Rajendran <nirmal@ad.unc.edu>
2024-02-15 13:14:38 -0500

    Add routes for catform and dogform

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import {Login} from './pages/Login'
 import {PetForm} from './pages/PetForm'
+import {CatForm} from './pages/CatForm'
+import {DogForm} from './pages/DogForm'
 
 export const RoutesApp = () => {
     return (
@@ -8,6 +10,8 @@ export const RoutesApp = () => {
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/petform" element={<PetForm />} />
+                <Route path="/dogform" element={<DogForm />} />
+                <Route path="/catform" element={<CatForm />} />
             </Routes>
         </Router>
     )
