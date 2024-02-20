252eacdf5276cab41442d9542b688f125d447b34
Denthin <christinehu.read@gmail.com>
2024-02-20 12:56:03 -0500

    Implement createUser()

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,24 @@
+import { db } from "../firebase-config.js";
+import { collection, addDoc} from "firebase/firestore";
 export const Login = () => {
+    userRef = "users";
+    createFirebaseUser = async (f_name, l_name, addr, countr, cit, sta, z_code, h_phone, c_phone, email_addr) => {
+        newUser = {
+            first_name: f_name, 
+            last_name: l_name, 
+            address: addr, 
+            country: countr, 
+            city: cit, 
+            state: sta, 
+            zip_code: z_code, 
+            home_phone: h_phone, 
+            cell_phone: c_phone, 
+            email: email_addr
+        };
+        newUserId = await addDoc(collection(db, userRef), newUser);
+        return newUserId;
+    };
+
     return (
         <div>
         <header>Welcome to Beautiful Together</header>
