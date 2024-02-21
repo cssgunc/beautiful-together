b15fc4b5bc00a13a7ea6c718404833f413012149
HendersonDaniel <danielhenderson.codeprojects@gmail.com>
2024-02-20 19:01:04 -0500

    added stuff to test the function

--- src/pages/PetForm.js
+++ src/pages/PetForm.js
@@ -1,7 +1,37 @@
+import { addMailDocument } from "../responseEmail.js";
 export const PetForm = () => {
+
+    async function callAddMailDocument() {
+        try {
+            console.log("test");
+          await addMailDocument(
+            'John', 
+            'Doe', 
+            '123 Main St', 
+            'USA', 
+            'Anytown', 
+            'CA',
+            '90001', 
+            '555-1234', 
+            '555-5678', 
+            'john.doe@example.com' 
+          );
+          console.log('email sent');
+        } catch (error) {
+          console.error('email not sent', error);
+        }
+      }
+
+      
     return (
         <div>
             <header>Pet Preference Form</header>
+            
+            <button onClick={() => callAddMailDocument()}>Click Me</button>
         </div>
+
     );
+
+
+    
 }
--- src/responseEmail.js
+++ src/responseEmail.js
@@ -11,18 +11,18 @@ async function addMailDocument(first_name, last_name, address, country, city, st
         subject: 'Adoption Request Notice',
         text: 'Notice:',
         html: 
-        <><p>Hello,</p><p>We have received an adoption request from the following individual:</p><ul>
-                <li><strong>First Name:</strong> ${first_name}</li>
-                <li><strong>Last Name:</strong> ${last_name}</li>
-                <li><strong>Address:</strong> ${address}</li>
-                <li><strong>Country:</strong> ${country}</li>
-                <li><strong>City:</strong> ${city}</li>
-                <li><strong>State:</strong> ${state}</li>
-                <li><strong>Zip Code:</strong> ${zip_code}</li>
-                <li><strong>Home Phone:</strong> ${home_phone}</li>
-                <li><strong>Cell Phone:</strong> ${cell_phone}</li>
-                <li><strong>Email:</strong> ${email}</li>
-            </ul><p>Thank you for your attention.</p></>,
+        `<p>Hello,</p><p>We have received an adoption request from the following individual:</p><ul>` +
+                `<li><strong>First Name:</strong> ${first_name}</li>` +
+                `<li><strong>Last Name:</strong> ${last_name}</li>` +
+                `<li><strong>Address:</strong> ${address}</li>` +
+                `<li><strong>Country:</strong> ${country}</li>` +
+                `<li><strong>City:</strong> ${city}</li>` +
+                `<li><strong>State:</strong> ${state}</li>` +
+                `<li><strong>Zip Code:</strong> ${zip_code}</li>` +
+                `<li><strong>Home Phone:</strong> ${home_phone}</li>` +
+                `<li><strong>Cell Phone:</strong> ${cell_phone}</li>` +
+                `<li><strong>Email:</strong> ${email}</li>` +
+            `</ul><p>Thank you for your attention.</p>`,
       },
       to: ['example@gmail.com'], //put test email here
     };
