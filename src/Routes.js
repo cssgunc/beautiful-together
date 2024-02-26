c651bb4b954462e976fea9c9cb5b50d42bac04c6
Nirmal Rajendran <nirmal@ad.unc.edu>
2024-02-26 18:56:57 -0500

    Add pet selection page

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import {Login} from './pages/Login'
 import {PetForm} from './pages/PetForm'
 import {CatForm} from './pages/CatForm'
 import {DogForm} from './pages/DogForm'
+import {PetSelection} from './pages/PetSelection'
 
 export const RoutesApp = () => {
     return (
@@ -12,6 +13,7 @@ export const RoutesApp = () => {
                 <Route path="/petform" element={<PetForm />} />
                 <Route path="/dogform" element={<DogForm />} />
                 <Route path="/catform" element={<CatForm />} />
+                <Route path="/petselection" element={<PetSelection />} />
             </Routes>
         </Router>
     )
---
+++ src/pages/PetSelection.js
@@ -0,0 +1,12 @@
+export const PetSelection = () => {
+
+    return (
+        <div>
+            <header>Pet Selection Page</header>
+        </div>
+
+    );
+
+
+    
+}
