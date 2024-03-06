6f826382351f140435ed5b4824d698f573a23b0a
Denthin <christinehu.read@gmail.com>
2024-03-05 20:01:35 -0500

    Disable buttons during animation

--- src/pages/PetSelection.js
+++ src/pages/PetSelection.js
@@ -8,7 +8,19 @@ export const PetSelection = () => {
         oldElement.classList.add("swipe" + side);
         newElement.classList.remove("background");
 
+        // To ensure the first background page has disabled buttons; adding 'disabled' to the button's html makes it irremovable.
+        var newLeft = document.getElementById(newId + "left");
+        var newRight = document.getElementById(newId + "right");
+        newLeft.disabled = true;
+        newRight.disabled = true;
+
+        var oldLeft = document.getElementById(oldId + "left");
+        var oldRight = document.getElementById(oldId + "right");
+        oldLeft.disabled = true;
+        oldRight.disabled = true;
+
         setTimeout(() => hide(oldId), 1000);
+        setTimeout(() => enableButtons(newId), 2000);
     }
 
     const hide = (id) => {
@@ -16,6 +28,13 @@ export const PetSelection = () => {
         element.classList.remove("swipeleft");
         element.classList.remove("swiperight");
         element.classList.add("background");
+    }
+
+    const enableButtons = (id) => {
+        var leftButton = document.getElementById(id + "left");
+        var rightButton = document.getElementById(id + "right");
+        leftButton.disabled = false;
+        rightButton.disabled = false;
     }
     
     return (
