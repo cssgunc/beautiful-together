55a7868fc979ee6bc07e02087a333de13153eaaf
Elaine Dong <elaine13@email.unc.edu>
2024-03-05 21:35:32 -0500

    implement home page frontend

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          green: '#47AA2C',
+          orange: {
+            200: '#F4A23C',
+            400: '#F78E1F',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
