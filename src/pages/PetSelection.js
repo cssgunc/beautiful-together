b4e01ed8daccc359fc210d5e9e41fd3ebd4d4403
Michael <140859932+michael-jnk@users.noreply.github.com>
2024-03-10 19:05:44 -0400

    html shell with notes

--- src/pages/PetSelection.js
+++ src/pages/PetSelection.js
@@ -1,10 +1,24 @@
 export const PetSelection = () => {
 
-    return (
-        <div>
-            <header>Pet Selection Page</header>
-        </div>
+    return true ? (
+        
+        <main> {/* side-background for non-mobile users */}
+            <div> {/* actual tinder slide, scaled to viewport height */}
+                <div></div> {/* bars at top */}
+                <div></div> {/* brief pet name & stats */}
+                <div></div> {/* container for bottom-row buttons */}
+            </div>
+            <div> {/* gray screen over for info screen (hidden initially) */}
+                <div></div> {/* pet info popup */}
+            </div>
+        </main>
 
+    )
+    : (
+        <main>
+            <img></img> {/* heartbreak pic */}
+            <div></div> {/* out of pets text */}
+        </main>
     );
 
 
